bed52f1550505a043e930375043aa89993fbabac
duanhuating <2500010135@qq.com>
2021-04-14 14:02:11 +0800

    完成了个人中心的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,50 @@
+$(function() {
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称长度要在6个之内';
+            }
+        }
+    })
+    inituserInfor()
+
+    function inituserInfor() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！');
+                }
+                // layer.msg('获取用户信息成功！')
+                form.val('formUserInfo', res.data);
+            }
+        })
+    }
+    $('#btnReset').on('click', function(e) {
+            e.preventDefault();
+            inituserInfor()
+        })
+        //监听表单的提交事件
+    $('.layui-form').on('submit', function(e) {
+        //阻止表单的默认提交行为
+        e.preventDefault();
+        //发起请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败')
+                }
+                layer.msg('更新用户信息成功！')
+                window.parent.getuserInfor();
+            }
+        })
+    })
+
+
+})
